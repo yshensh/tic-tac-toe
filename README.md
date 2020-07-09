5299d70ccabaf93f3f3db246132be06bb33f7dc9
Yi Shen <yshensh@gmail.com>
2020-07-09 18:30:57 -0400

    implement time travel feature

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 Build an interactive tic-tac-toe game with React by following [Tutorial: Intro to React](https://reactjs.org/tutorial/tutorial.html). <br />
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+### Features
+- Indicates when a player has won the game
+- Stores a game's history as a game progresses
+- Allows players to review a game's history and see previous versions of a game's board
+
+### To-do features (In Progress)
+- Display the location for each move in the format (col, row) in the move history list.
+- Bold the currently selected item in the move list.
+- Rewrite Board to use two loops to make the squares instead of hardcoding them.
+- Add a toggle button that lets you sort the moves in either ascending or descending order.
+- When someone wins, highlight the three squares that caused the win.
+- When no one wins, display a message about the result being a draw.
+- Rewrite in TypeScript
+
 ### Start the app 
 `npm start`
 
@@ -10,4 +24,3 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
 The page will reload if you make edits.<br />
 You will also see any lint errors in the console.
-
--- src/index.js
+++ src/index.js
@@ -25,51 +25,18 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      // each time a player moves, xIsNext will be flipped to determine which player goes next and the game's state will be saved
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    // ignore a click if someone has won the game or if a square is already filled
-    if (calculateWinner(squares) || squares[i]) {
-      return
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      // flip the value of xIsNext
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
   renderSquare(i) {
     return (
       <Square 
-        value={this.state.squares[i]} 
-        onClick={() => this.handleClick(i)}  
+        value={this.props.squares[i]} 
+        onClick={() => this.props.onClick(i)}  
       />
     );
   }
 
   render() {
-    // change the "status" text in Board's render so that it displays which player has the next turn
-    let status;
-    const winner = calculateWinner(this.state.squares);
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
-          <div>
-        <div className="status">{status}</div>
+      <div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -91,15 +58,87 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  // set up the initial state for the Game compoenent within in its constructor. This gives the Game component full control over the Board's data, and lets it instruct the Board to render previous turns from the 'history'  
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      // each time a player moves, xIsNext will be flipped to determine which player goes next and the game's state will be saved
+      xIsNext: true,
+      // indicate which step we're currently viewing
+      stepNumber: 0,
+    };
+  }
+
+  handleClick(i) {
+    // This ensures that if we "go back in time" and then make a new move from that point, we throw away all the "future" history that would now become incorrect. 
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    // ignore a click if someone has won the game or if a square is already filled
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      // flip the value of xIsNext
+      xIsNext: !this.state.xIsNext,
+      // after making a new move, we need to update stepNumber by adding 'history.length' as part of the this.setState argument. This ensures we don't get stuck showing the same move after a new one has been made
+      stepNumber: history.length,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
   render() {
+    // use the most recent history entry to determine and display the game's status
+    const history = this.state.history;
+    // rendering the currently selected move according to 'stepNumber'
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map(
+      (step, move) => {
+        const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+        return (
+          // assign proper key when building a dynamic lists
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
+
+    // change the "status" text in Board's render so that it displays which player has the next turn
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
